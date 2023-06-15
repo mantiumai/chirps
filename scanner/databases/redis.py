4b0ffc02913916b3ea28815cce6f1b255dbdfde8
Alex Nork <alex.nork@mantiumai.com>
2023-06-15 15:08:01 -0400

    add redis query

--- scanner/databases/redis.py
+++ scanner/databases/redis.py
@@ -1,6 +1,8 @@
 from os import getenv
 
-from redis.client import Redis
+import numpy as np
+from redis import Redis
+from redis.commands.search.query import Query
 
 
 class RedisDocumentStore:
@@ -13,6 +15,9 @@ class RedisDocumentStore:
         database: int | None = None,
         username: str | None = None,
         password: str | None = None,
+        index_name: str | None = None,
+        distance_metric: str = 'cosine',
+        embedding_field: str = 'embeddings',
     ) -> None:
         """Initialize the RedisDocumentStore."""
         self.host = host or getenv('REDIS_HOST', 'localhost')
@@ -22,4 +27,38 @@ class RedisDocumentStore:
         self.password = password or getenv('REDIS_PASSWORD')
 
         # Instantiate a client to the Redis server for this destination
-        self.client = Redis(host=host, port=port, db=database, password=password, username=username)    # type: ignore
+        self.client = Redis(
+            host=self.host,  # type: ignore
+            port=self.port,  # type: ignore
+            db=self.database,  # type: ignore
+            password=self.password,
+            username=self.username,
+        )
+
+        self.index_name = index_name
+        self.index = self.client.ft(self.index_name)    # type: ignore
+        self.distance_metric = distance_metric
+        self.embedding_field = embedding_field
+
+    def _scale_to_unit_interval(self, score: float) -> float:
+        # cosine is the only supported distance metric
+        if self.distance_metric == 'cosine':
+            return (score + 1) / 2
+
+        return score
+
+    def query_by_embedding(self, query_emb: list[float], top_k: int) -> list[str]:
+        """Query documents by embedding."""
+        score_field = 'vec_score'
+        vector_param = 'vec_param'
+
+        vss_query = f'*=>[KNN {top_k} @{self.embedding_field} ${vector_param} AS {score_field}]'
+        return_fields = [self.embedding_field, 'document_id', 'sync_file_id', score_field]
+
+        query = Query(vss_query).sort_by(score_field).paging(0, top_k).return_fields(*return_fields).dialect(2)
+        embedding = np.array(query_emb, dtype=np.float32).tostring()    # type: ignore
+        params: dict[str, float] = {vector_param: embedding}
+        results = self.index.search(query, query_params=params)
+
+        embeddings = [doc.embeddings for doc in results.docs]
+        return embeddings
