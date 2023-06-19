21ee32e24286f8fe21a8d13496973c2e1759c1d9
Alex Nork <alex.nork@mantiumai.com>
2023-06-19 13:11:07 -0400

    load employee scan in test

--- tests/providers/test_base_provider.py
+++ tests/providers/test_base_provider.py
@@ -6,18 +6,20 @@ from mantium_scanner.providers.base import BaseProvider
 
 
 @pytest.fixture(params=['employee.json'])
-def load_scan(request):
+def scans(request):
     with open(f'mantium_scanner/profiles/{request.param}') as f:
         profile = json.load(f)
 
         scans = profile['scans']
-        assert scans
+        return scans
 
 
-def test_match_pattern(load_scan):
-    doc_text = ['123-456-7890', 'the 1 quick 2brown fox 34-56', '123-456-7890', '11-2222-34123']
-    pattern = r'\d{3}-\d{3}-\d{4}'
+def test_match_pattern(scans):
+    doc_text = ['123-45-6789', 'the 1 quick 2brown fox 34-56', '987-65-4321', '11-2222-34123']
 
-    provider = BaseProvider()
-    matches = provider.match_pattern(doc_text, pattern)
-    assert matches == ['123-456-7890', '123-456-7890']
+    for scan in scans:
+        patterns = scan['flags']
+        for pattern in patterns:
+            provider = BaseProvider()
+            matches = provider.match_pattern(doc_text, pattern)
+            assert matches == ['123-45-6789', '987-65-4321']
