b6efaca16dd2bf7059772764ffa33459195ba7e7
Alex Nork <alex.nork@mantiumai.com>
2023-06-19 16:01:32 -0400

    make doc text in test more realistic

--- tests/providers/test_base_provider.py
+++ tests/providers/test_base_provider.py
@@ -18,21 +18,31 @@ def scans(request):
 def employee_profile_scans(scans):
     return {
         'Employee SSN': {
-            'doc_text': ['123-45-6789', 'the 1 quick 2brown fox 34-56', '987-65-4321', '11-2222-34123'],
+            'doc_text': [
+                'John Doe, SSN: 123-45-6789, Position: Manager',
+                'The quick brown fox jumps over the lazy dog 34-56',
+                'Mary Johnson, SSN: 987-65-4321, Position: Engineer',
+                'Invalid SSN: 11-2222-34123',
+            ],
             'expected_matches': ['123-45-6789', '987-65-4321'],
         },
         'Credit Card Number': {
             'doc_text': [
-                'something 4111111111111111 something',
-                'the 1 quick 2brown fox 34-56',
-                '6011111111111117',
-                '5555555555554444',
+                'Payment details: Visa card 4111111111111111, expiration 02/25',
+                'The quick brown fox jumps over the lazy dog',
+                'Discover card for reservation: 6011111111111117, expiration 05/24',
+                'Using MasterCard for purchase: 5555555555554444, expiration 12/23',
             ],
             'expected_matches': ['4111111111111111', '6011111111111117', '5555555555554444'],
         },
         'Employee Bank Account': {
-            'doc_text': ['123456789', 'the 1 quick 2brown fox 34-56', '987654321', '1112222333'],
-            'expected_matches': ['123456789', '987654321', '1112222333'],
+            'doc_text': [
+                'Salary transfer for John Doe: Bank account 123456789, routing 123456789',
+                'The quick brown fox jumps over the lazy dog',
+                'Mary Johnson: Direct deposit to bank account 987654321, routing 987654321',
+                'Payroll account for company: 1112222333, routing 111222333',
+            ],
+            'expected_matches': ['123456789', '987654321', '1112222333', '111222333'],
         },
     }
 
