b03d18beb20981467dad290761cc4563cda9b095
Alex Nork <alex.nork@mantiumai.com>
2023-06-20 08:28:45 -0400

    add back alembic directory and files

---
+++ alembic/versions/alembic/versions/6b6393c8233a_add_user_table.py
@@ -0,0 +1,38 @@
+"""
+add user table
+
+Revision ID: 6b6393c8233a
+Revises:
+Create Date: 2023-06-19 23:12:37.067153
+"""
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = '6b6393c8233a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    """Create the user table"""
+    op.create_table(
+        'users',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('username', sa.String(), nullable=True),
+        sa.Column('hashed_password', sa.String(), nullable=True),
+        sa.PrimaryKeyConstraint('id'),
+    )
+    op.create_index(op.f('ix_users_id'), 'users', ['id'], unique=False)
+    op.create_index(op.f('ix_users_username'), 'users', ['username'], unique=True)
+    # ### end Alembic commands ###
+
+
+def downgrade() -> None:
+    """Drop the user table"""
+    op.drop_index(op.f('ix_users_username'), table_name='users')
+    op.drop_index(op.f('ix_users_id'), table_name='users')
+    op.drop_table('users')
+    # ### end Alembic commands ###
