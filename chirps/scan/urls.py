790049a28596a59f068d7a20b69c9d64e92dd568
Rob Zimmerman <rob@thezims.net>
2023-07-07 20:25:56 +0000

    scan status polling

--- chirps/scan/urls.py
+++ chirps/scan/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.dashboard, name='scan_dashboard'),
     path('create/', views.create, name='scan_create'),
     path('result/<int:result_id>/', views.result_detail, name='result_detail'),
-    path('finding/<int:finding_id>/', views.finding_detail, name='finding_detail')
+    path('finding/<int:finding_id>/', views.finding_detail, name='finding_detail'),
+    path('status/<int:scan_id>/', views.status, name='scan_status'),
 ]
