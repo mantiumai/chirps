1dd8717820743f451b5d4fd06542b04d45ac81bf
joshjtaylor <josh@joshjtaylor.com>
2023-07-07 19:00:31 +0000

    Updated bootstrap theming + complete UI overhaul

--- chirps/chirps/settings.py
+++ chirps/chirps/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'base_app/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
