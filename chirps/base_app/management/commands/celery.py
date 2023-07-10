05ab6b8436ec49946ac57afe59d8c691d2b6d181
Alex Nork <48630278+alex-nork@users.noreply.github.com>
2023-07-10 20:08:06 +0000

    revert unintended changes and fix typos

--- chirps/base_app/management/commands/celery.py
+++ chirps/base_app/management/commands/celery.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
     help = 'Interact with the local celery broker'
 
     def add_arguments(self, parser):
-        """Add arguments to rabbitmq command"""
+        """Add arguments to celery command"""
         parser.add_argument('--start', action='store_true', help='Start celery server')
         parser.add_argument('--stop', action='store_true', help='Stop celery server')
         parser.add_argument('--restart', action='store_true', help='Restart celery server')
