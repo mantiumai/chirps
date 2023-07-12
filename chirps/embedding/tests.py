ff6ab993ac78188474191d2ff3f4cbd99aad83b8
Rob Zimmerman <rob@thezims.net>
2023-07-12 10:59:02 +0000

    fixing up tests

--- chirps/embedding/tests.py
+++ chirps/embedding/tests.py
@@ -28,6 +28,9 @@ class TestEmbedding(TestCase):
             reverse('signup'), {'username': username, 'email': email, 'password1': password, 'password2': password}
         )
 
+        # Set a dummy OpenAI key
+        self.client.post(reverse('profile'), {'openai_key': 'test_openai_key'})
+
     def test_create_invalid(self):
         """Test creating an embedding with invalid URL parameters."""
         # Missing text field should yield a 400 response
