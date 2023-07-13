e4a8fea18a76047d42a96fcc45370b1394adbde0
Alex Nork <48630278+alex-nork@users.noreply.github.com>
2023-07-13 20:39:37 +0000

    reformat redis provider

--- chirps/target/providers/redis.py
+++ chirps/target/providers/redis.py
@@ -14,7 +14,7 @@ class RedisTarget(BaseTarget):
     """Implementation of a Redis target."""
 
     host = models.CharField(max_length=1048)
-    port = models.IntegerField()
+    port = models.PositiveIntegerField()
     database_name = models.CharField(max_length=256)
     username = models.CharField(max_length=256)
     password = models.CharField(max_length=2048, blank=True, null=True)
@@ -37,23 +37,26 @@ class RedisTarget(BaseTarget):
             password=self.password,
             username=self.username,
         )
-        index = client.ft(self.index_name)
+        try:
+            index = client.ft(self.index_name)
 
-        score_field = 'vec_score'
-        vector_param = 'vec_param'
+            score_field = 'vec_score'
+            vector_param = 'vec_param'
 
-        vss_query = f'*=>[KNN {max_results} @{self.embedding_field} ${vector_param} AS {score_field}]'
-        return_fields = [self.embedding_field, self.text_field, score_field]
+            vss_query = f'*=>[KNN {max_results} @{self.embedding_field} ${vector_param} AS {score_field}]'
+            return_fields = [self.embedding_field, self.text_field, score_field]
 
-        search_query = Query(vss_query).sort_by(score_field).paging(0, max_results).return_fields(*return_fields).dialect(2)
-        embedding = np.array(query, dtype=np.float32).tostring()    # type: ignore
-        params: dict[str, float] = {vector_param: embedding}
-        results = index.search(search_query, query_params=params)
+            search_query = (
+                Query(vss_query).sort_by(score_field).paging(0, max_results).return_fields(*return_fields).dialect(2)
+            )
+            embedding = np.array(query, dtype=np.float32).tostring()
+            params: dict[str, float] = {vector_param: embedding}
+            results = index.search(search_query, query_params=params)
 
-        print(f'results: {results.docs}')
-
-        docs = [doc.content for doc in results.docs]
-        return docs
+            docs = [doc[f'{self.text_field}'] for doc in results.docs]
+            return docs
+        finally:
+            client.close()
 
     def test_connection(self) -> bool:
         """Ensure that the Redis target can be connected to."""
@@ -64,4 +67,7 @@ class RedisTarget(BaseTarget):
             password=self.password,
             username=self.username,
         )
-        return client.ping()
+        try:
+            return client.ping()
+        finally:
+            client.close()
