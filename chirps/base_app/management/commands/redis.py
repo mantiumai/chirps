60e1cf7227eb84171057f631c6e8024237a9f012
Alex Nork <48630278+alex-nork@users.noreply.github.com>
2023-07-13 01:56:10 +0000

    run pre-commit

--- chirps/base_app/management/commands/redis.py
+++ chirps/base_app/management/commands/redis.py
@@ -42,16 +42,16 @@ class Command(BaseCommand):
 #         parser.add_argument('--stop', action='store_true', help='Stop redis server')
 #         parser.add_argument('--status', action='store_true', help='Check redis server status')
 
-#     def handle(self, *args, **options):  
-#         """Handle redis command"""  
-#         docker_compose_path = shutil.which("docker-compose")  
-#         if not docker_compose_path:  
-#             self.stdout.write(self.style.ERROR("docker-compose not found. Please ensure it is installed."))  
-#             return  
-  
-#         if options['start']:  
-#             os.system(f'{docker_compose_path} -f /workspace/.devcontainer/docker-compose.yml up -d redis')  
-#         elif options['stop']:  
-#             os.system(f'{docker_compose_path} -f /workspace/.devcontainer/docker-compose.yml down')  
-#         elif options['status']:  
-#             os.system(f'{docker_compose_path} -f /workspace/.devcontainer/docker-compose.yml ps') 
+#     def handle(self, *args, **options):
+#         """Handle redis command"""
+#         docker_compose_path = shutil.which("docker-compose")
+#         if not docker_compose_path:
+#             self.stdout.write(self.style.ERROR("docker-compose not found. Please ensure it is installed."))
+#             return
+
+#         if options['start']:
+#             os.system(f'{docker_compose_path} -f /workspace/.devcontainer/docker-compose.yml up -d redis')
+#         elif options['stop']:
+#             os.system(f'{docker_compose_path} -f /workspace/.devcontainer/docker-compose.yml down')
+#         elif options['status']:
+#             os.system(f'{docker_compose_path} -f /workspace/.devcontainer/docker-compose.yml ps')
