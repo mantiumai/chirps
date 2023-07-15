b50a2b609f75306ff52f0958a346c24ba2cb8e64
Alex Nork <48630278+alex-nork@users.noreply.github.com>
2023-07-15 21:10:53 +0000

    add test

--- chirps/base_app/tests.py
+++ chirps/base_app/tests.py
@@ -1,1 +1,8 @@
 """Tests for the base application."""
+from django.test import TestCase
+from .management.commands.initialize_app import Command
+
+class InitializeAppTests(TestCase):
+    def test_load_data_from_plans_directory(self):
+        command = Command()
+        command.load_data_from_plans_directory()
