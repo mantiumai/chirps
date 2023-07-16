f0b36415fd1c7490b7327859727d2718ea72712c
Alex Nork <48630278+alex-nork@users.noreply.github.com>
2023-07-16 01:45:27 +0000

    add ssn and bank account tests. fix bank account regex

--- chirps/plan/tests.py
+++ chirps/plan/tests.py
@@ -1,11 +1,80 @@
 """Tests for the plan application."""
 
+import re
 from unittest import skip
 
 from django.test import TestCase
 from django.urls import reverse
 
 from .forms import PlanForm
+from .models import Plan, PlanVersion, Rule
+
+
+class EmployeeRegexTests(TestCase):
+    """Test rule regex"""
+
+    fixtures = ['plan/employee.json']
+    
+    def setUp(self):
+        # Query the Plan with the name "Employee Information"  
+        employee_information_plan = Plan.objects.get(name="Employee Information")  
+        
+        # Get the current PlanVersion associated with the retrieved plan  
+        current_plan_version = employee_information_plan.current_version  
+        
+        # Get all the rules associated with the current PlanVersion  
+        self.rules = Rule.objects.filter(plan=current_plan_version)  
+        
+        # Print the retrieved rules  
+        for rule in self.rules:  
+            print(rule)  
+  
+    
+    def test_social_security_number(self):  
+        rule = self.rules.get(name='SSN')  
+        pattern = re.compile(rule.regex_test)  
+    
+        example = 'this is a test. the number is 123-45-6789. the end!'  
+        self.assertIsNotNone(pattern.search(example))  
+
+    # def test_address(self):
+    #     rule = self.rules.get(name='Address')
+    #     pattern = re.compile(rule.regex_test)
+
+    #     valid_addresses = [  
+    #         # "123 Main St, CA 90210",  
+    #         "456 Elm St, New York, NY 10001",  
+    #         "789 Oak St, Los Angeles, CA 90001-1234",  
+    #     ]  
+  
+    #     invalid_addresses = [  
+    #         "12345 67890, NY 10001",  
+    #         "Main St, CA 90210",  
+    #         "123 Main St, California 90210",  
+    #     ]  
+    
+    #     for address in valid_addresses:  
+    #         self.assertIsNotNone(pattern.search(address), address)  
+    
+    #     for address in invalid_addresses:  
+    #         self.assertIsNone(pattern.search(address), address)  
+
+        
+    #     example = "The address is 123 Main Street, Springfield, OH. That's the spot."
+    #     self.assertIsNotNone(pattern.search(example))
+
+    def test_bank_account_number(self):
+        rule = self.rules.get(name='Bank Account')
+        pattern = re.compile(rule.regex_test)
+
+        valid_account_numbers = [  
+            "123-4567-8901234",  
+            "987-1234-5678901",  
+            "001-0001-0000000",  
+        ]
+
+        for account_number in valid_account_numbers:  
+            self.assertIsNotNone(pattern.match(account_number), account_number)
 
 
 @skip('Disabling until pagination is re-added to the plan application.')
