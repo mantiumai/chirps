7510fec7b965dea0ddead679a67286ae58102105
Alex Nork <48630278+alex-nork@users.noreply.github.com>
2023-07-19 13:33:01 +0000

    change name in base_app

--- chirps/base_app/tests.py
+++ chirps/base_app/tests.py
@@ -1,6 +1,6 @@
 """Tests for the base application."""
 from django.test import TestCase
-from plan.models import Plan
+from policy.models import Policy
 
 from .management.commands.initialize_app import Command
 
@@ -8,11 +8,11 @@ from .management.commands.initialize_app import Command
 class InitializeAppTests(TestCase):
     """Test the initialize_app management command"""
 
-    def test_load_data_from_plans_directory(self):
+    def test_load_data_from_policies_directory(self):
         """Test that data is loaded"""
-        assert Plan.objects.count() == 0
+        assert Policy.objects.count() == 0
         command = Command()
-        command.load_data_from_plans_directory()
+        command.load_data_from_policies_directory()
 
         # check that data was loaded
-        assert Plan.objects.count() > 0
+        assert Policy.objects.count() > 0
