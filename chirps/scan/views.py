c5e12d032d2a4756736d9813d3d0df0a41357e1d
Rob Zimmerman <rob.zimmerman@mantiumai.com>
2023-07-26 09:41:59 -0400

    removing extra call to kick off a task

--- chirps/scan/views.py
+++ chirps/scan/views.py
@@ -113,9 +113,6 @@ def create(request):
             selected_policies = scan_form.cleaned_data['policies']
             scan.policies.set(selected_policies)
 
-            # Kick off the scan task
-            result = scan_task.delay(scan.id)
-
             # For every target that was selected, kick off a task
             for target in scan_form.cleaned_data['targets']:
 
