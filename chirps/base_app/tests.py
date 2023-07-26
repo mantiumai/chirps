d742c3ed9edcd0403050971207835c0978254901
Alex Nork <alex.nork@mantiumai.com>
2023-07-26 12:46:12 +0000

    pass path to function

--- chirps/base_app/tests.py
+++ chirps/base_app/tests.py
@@ -1,10 +1,13 @@
 """Tests for the base application."""
+from pathlib import Path
 from unittest import skip
 
 from django.test import TestCase
 from policy.models import Policy
 
 from .management.commands.initialize_app import Command
+
+fixture_path = Path(__file__).parent.resolve() / Path('./fixtures/embedding')
 
 
 class InitializeAppTests(TestCase):
@@ -15,7 +18,7 @@ class InitializeAppTests(TestCase):
         """Test that data is loaded"""
         assert Policy.objects.count() == 0
         command = Command()
-        command.load_data_from_fixtures()
+        command.load_data_from_fixtures(fixture_path)
 
         # check that data was loaded
         assert Policy.objects.count() > 0
