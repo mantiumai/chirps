e32f2c4a9e97133365f53376d2ba1fdf011a005c
Alex Nork <alex.nork@mantiumai.com>
2023-08-04 00:19:47 +0000

    add generate_embeddings command

---
+++ chirps/base_app/management/commands/generate_embeddings.py
@@ -0,0 +1,105 @@
+import json
+import os
+import sys
+
+import cohere
+import numpy as np
+import openai
+from django.apps import apps
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Custom Django management command for generating embeddings using the specified service and model."""
+
+    help = 'Generate embeddings for the input data'
+
+    def add_arguments(self, parser):
+        """Define and add the required arguments for the command."""
+        parser.add_argument('service', type=str, help='Service to use for embeddings (either "OpenAI" or "cohere")')
+        parser.add_argument('api_key', type=str, help='API key for the selected service')
+        parser.add_argument('app_name', type=str, help='Django app name')
+        parser.add_argument('model_name', type=str, help='Model name for the selected service')
+
+    def handle(self, *args, **options):
+        """Process the input data, generate embeddings using the specified service and model, and save the results."""
+        service = options['service'].lower()
+        api_key = options['api_key']
+        app_name = options['app_name']
+        model_name = options['model_name']
+
+        Embedding = apps.get_model('embedding', 'Embedding')
+
+        # Check if the provided service is valid
+        if service not in Embedding.Service.values:
+            sys.exit('Invalid service. Please choose one of the following: ' + ', '.join(Embedding.Service.values))
+
+        # Check if the provided model is valid for the selected service
+        available_models = Embedding.get_models_for_service(service)
+        available_model_names = [model[0] for model in available_models]
+        if model_name not in available_model_names:
+            sys.exit(
+                'Invalid model. Please choose one of the following for the selected service: '
+                + ', '.join(available_model_names)
+            )
+
+        if service == 'openai':
+            openai.api_key = api_key
+        elif service == 'cohere':
+            cohere_client = cohere.Client(api_key)
+
+        next_pk = Embedding.objects.order_by('-pk').first().pk + 1
+
+        base_path = settings.BASE_DIR.as_posix()
+        subdirectory = f'{base_path}/{app_name}/fixtures/{app_name}'
+
+        for root, dirs, files in os.walk(subdirectory):
+            for file in files:
+                if file.endswith('.json'):
+                    file_path = os.path.join(root, file)
+
+                    with open(file_path, 'r') as json_file:
+                        data = json.load(json_file)
+
+                        embeddings_data = []
+                        for datum in data:
+                            if 'query_string' in datum['fields']:
+                                query_string = datum['fields']['query_string']
+
+                                # Check if an embedding already exists
+                                existing_embedding = Embedding.objects.filter(
+                                    service=Embedding.Service(service).label, model=model_name, text=query_string
+                                ).first()
+
+                                if existing_embedding:
+                                    print(f'Embedding already exists for {query_string}')
+                                    continue
+
+                                if service == 'openai':
+                                    response = openai.Embedding.create(engine=model_name, input=query_string)
+                                    embeddings = response.data[0].embedding
+                                elif service == 'cohere':
+                                    response = cohere_client.embed(texts=[query_string], model=model_name)
+                                    embeddings = response.embeddings[0]
+
+                                embedding_array = np.array(embeddings)
+                                print(embedding_array)
+
+                                data_to_write = {
+                                    'model': 'embedding.embedding',
+                                    'pk': next_pk,
+                                    'fields': {
+                                        'created_at': '2022-01-01T00:00:00Z',
+                                        'model': model_name,
+                                        'service': Embedding.Service(service).label,
+                                        'text': query_string,
+                                        'vectors': [f'{value:.15f}' for value in embedding_array],
+                                    },
+                                }
+                                embeddings_data.append(data_to_write)
+                                next_pk += 1
+
+                        new_file_name = f'{service}_rules.json'
+                        with open(f'{base_path}/{app_name}/fixtures/embedding/' + new_file_name, 'w') as f:
+                            json.dump(embeddings_data, f, indent=4)
