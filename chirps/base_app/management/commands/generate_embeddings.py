f5b37504f0cd36875d9150c6ab96eaa2ad785473
Alex Nork <alex.nork@mantiumai.com>
2023-08-04 18:09:53 +0000

    refactor command to not write file before saving Embedding.

--- chirps/base_app/management/commands/generate_embeddings.py
+++ chirps/base_app/management/commands/generate_embeddings.py
@@ -1,17 +1,17 @@
 """Generate embeddings for policy rules from policy fixtures"""
 
+import getpass
 import json
 import os
 import sys
 
 import cohere
-import numpy as np
 import openai as openai_client
-from django.apps import apps
 from django.conf import settings
-from django.core.management import call_command
+from django.contrib.auth import authenticate
 from django.core.management.base import BaseCommand, CommandError
 from embedding.models import Embedding
+from embedding.providers.base import BaseEmbeddingProvider
 
 
 class Command(BaseCommand):
@@ -22,18 +22,27 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         """Define and add the required arguments for the command."""
         parser.add_argument('service', type=str, help='Service to use for embeddings (either "OpenAI" or "cohere")')
-        parser.add_argument('api_key', type=str, help='API key for the selected service')
         parser.add_argument('model_name', type=str, help='Model name for the selected service')
         parser.add_argument('-a', '--app_name', type=str, default='policy', help='Django app name (default: "policy")')
 
     def handle(self, *args, **options):
         """Handle generate embeddings command"""
         service = options['service']
-        api_key = options['api_key']
         app_name = options['app_name']
         model_name = options['model_name']
 
-        Embedding = apps.get_model('embedding', 'Embedding')
+        # prompt the user for their username and password
+        username = input('Enter your Chirps username: ')
+        password = getpass.getpass('Enter your password: ')
+
+        # authenticate the user
+        user = authenticate(username=username, password=password)
+        if user is None:
+            raise CommandError('Invalid username or password')
+
+        # retrieve the API key from the User object
+        service_keys = {Embedding.Service.OPEN_AI: 'openai_key', Embedding.Service.COHERE: 'cohere_key'}
+        api_key = getattr(user.profile, service_keys[service])
 
         # Check if the provided service is valid
         if service not in Embedding.Service.values:
@@ -67,23 +76,11 @@ class Command(BaseCommand):
                     with open(file_path, 'r', encoding='utf-8') as json_file:
                         data = json.load(json_file)
 
-                    embeddings_data = self.process_data(data, service, client, model_name, Embedding)
-                    if embeddings_data:
-                        new_file_name = self.create_new_file_name(service, file)
-                        new_file_path = os.path.join(base_path, 'embedding', 'fixtures', 'embedding', new_file_name)
-                        with open(new_file_path, 'w', encoding='utf-8') as f:
-                            json.dump(embeddings_data, f, indent=4)
+                    self.process_data(user, data, service, model_name)
 
-                        # Run the loaddata command
-                        call_command('loaddata', new_file_path)
-
-    def process_data(self, data, service, client, model_name):
+    def process_data(self, user, data, service, model_name):
         """Process the input data, generate embeddings, and return the embeddings_data."""
-        embeddings_data = []
         processed_combinations = set()
-        latest_embedding_record = Embedding.objects.order_by('-pk').first()
-        current_pk = latest_embedding_record.pk if latest_embedding_record else 0
-        next_pk = current_pk + 1
 
         for datum in data:
             if isinstance(datum, dict) and 'query_string' in datum['fields']:
@@ -104,30 +101,22 @@ class Command(BaseCommand):
                     processed_combinations.add(combination)
                     continue
 
-                if service == 'OpenAI':
-                    response = client.Embedding.create(engine=model_name, input=query_string)
-                    embeddings = response.data[0].embedding
-                elif service == 'cohere':
-                    response = client.embed(texts=[query_string], model=model_name)
-                    embeddings = response.embeddings[0]
+                provider: BaseEmbeddingProvider = Embedding.Service.get_provider_from_service_name(service)
 
-                embedding_array = np.array(embeddings)
-                data_to_write = {
-                    'model': 'embedding.embedding',
-                    'pk': next_pk,
-                    'fields': {
-                        'created_at': '2022-01-01T00:00:00Z',
-                        'model': model_name,
-                        'service': Embedding.Service(service).label,
-                        'text': query_string,
-                        'vectors': [f'{value:.15f}' for value in embedding_array],
-                    },
-                }
-                embeddings_data.append(data_to_write)
-                processed_combinations.add(combination)
-                next_pk += 1
+                # Use the specified service to generate embeddings with the specified model
+                # Raises EmbeddingError if the embedding fails
+                embed_result = provider.embed(user=user, model=model_name, text=query_string)
 
-        return embeddings_data
+                # Save the embedding result to the database
+                Embedding.objects.create(
+                    model=model_name,
+                    service=service,
+                    text=query_string,
+                    vectors=embed_result,
+                    user=user,
+                )
+
+                print(f'Created embedding: {query_string}')
 
     def create_new_file_name(self, service, file):
         """Create a new file name based on the service and original file name."""
