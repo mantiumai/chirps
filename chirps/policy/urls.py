0246b30c507330a165d2f7d638a0d131348ca86a
Alex Nork <alex.nork@mantiumai.com>
2023-08-24 13:14:45 +0000

    delete commented url

--- chirps/policy/urls.py
+++ chirps/policy/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('archive/<int:policy_id>', views.archive, name='policy_archive'),
     path('clone/<int:policy_id>', views.clone, name='policy_clone'),
     path('create/', views.create, name='policy_create'),
-    # path('create_rule/', views.create_rule, name='policy_create_rule'),
     path('create_rule/<str:rule_type>/', views.create_rule, name='policy_create_rule'),
     path('delete_rule/<int:rule_id>', views.delete_rule, name='policy_delete_rule'),
     path('edit/<int:policy_id>', views.edit, name='policy_edit'),
