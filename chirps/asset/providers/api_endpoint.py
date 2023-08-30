924ee62deede9aa2e997feb60116607bfda954b0
alex-nork <alexnork@gmail.com>
2023-08-30 16:49:25 +0000

    add description and timeout fields to the apiendpointasset model. refer to its timeout attribute when making the request in fetch_api_data

--- chirps/asset/providers/api_endpoint.py
+++ chirps/asset/providers/api_endpoint.py
@@ -14,6 +14,9 @@ logger = getLogger(__name__)
 class APIEndpointAsset(BaseAsset):
     """Implementation of an API Endpoint asset."""
 
+    # what is the model served by this asset supposed to be doing?
+    description = models.TextField(blank=True, null=True, max_length=2048)
+
     url = models.URLField(max_length=2048, blank=False, null=False)
     authentication_method = models.CharField(
         max_length=10, choices=[('Basic', 'Basic'), ('Bearer', 'Bearer')], default='Bearer'
@@ -21,6 +24,7 @@ class APIEndpointAsset(BaseAsset):
     api_key = EncryptedCharField(max_length=256, editable=True)
     headers = models.JSONField(blank=True, null=True)
     body = models.JSONField(blank=True, null=True, default={'data': '%query%'})
+    timeout = models.IntegerField(default=30)
 
     # Name of the file in the ./asset/static/ directory to use as a logo
     html_logo = 'asset/api-endpoint-logo.svg'
@@ -58,7 +62,7 @@ class APIEndpointAsset(BaseAsset):
 
         # Send the request
         try:
-            response = requests.post(self.url, headers=headers, json=body, timeout=15)
+            response = requests.post(self.url, headers=headers, json=body, timeout=self.timeout)
         except Timeout as exc:
             raise RequestException('Error: API request timed out') from exc
 
